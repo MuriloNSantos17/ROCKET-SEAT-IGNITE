5c314606a43ec0abde49b2126af1792d79c04873
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2023-10-29 12:58:42 -0300

    Commit some posts

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,12 +1,18 @@
-import {Post} from './components/Post';
+import { Post } from './components/Post';
 import { Fragment } from 'react';
 
 export function App() {
   return (
     <Fragment>
       <h1>Hello World</h1>
-      <Post />
+      <Post
+        author={'Murilo Santos'}
+        content={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, a."} />
+      <Post
+        author={'Nicolas Santos'}
+        content={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, a."} />
     </Fragment>
   )
 }
+
 
--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,10 @@
-export function Post() {
-    return <p>Post</p>
+import { Fragment } from "react"
+
+export function Post(props) {
+    return (
+        <Fragment>
+            <h1>Author: {props.author}</h1>
+            <p>{props.content}</p>
+        </Fragment>
+    )
 }
