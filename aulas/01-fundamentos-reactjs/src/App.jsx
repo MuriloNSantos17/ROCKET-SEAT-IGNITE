101aef693e5f7de8cc86cc6f4989e8d01c8222a1
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2023-10-29 12:35:53 -0300

    Commit first components

---
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,12 @@
+import {Post} from './components/Post';
+import { Fragment } from 'react';
+
+export function App() {
+  return (
+    <Fragment>
+      <h1>Hello World</h1>
+      <Post />
+    </Fragment>
+  )
+}
+
