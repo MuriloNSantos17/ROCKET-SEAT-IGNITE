33f5759fb1f94857dd1b5f24990a5958039fa6b0
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2023-11-04 12:04:59 -0300

    commit comment

--- aulas/feed-project/src/components/Comment.jsx
+++ aulas/feed-project/src/components/Comment.jsx
@@ -1,11 +1,19 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css'
+import { useState } from 'react';
 import Avatar from './Avatar';
 
 const Comment = ({ content, onDeleteComment }) => {
+    const [likeCount, setLikeCount] = useState(0);
 
     const handleDeleteComment = () => {
         onDeleteComment(content);
+    }
+
+    const handleLikeComment = () => {
+        setLikeCount((state) => {
+            return state + 1;
+        })
     }
 
     return (
@@ -29,9 +37,9 @@ const Comment = ({ content, onDeleteComment }) => {
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
