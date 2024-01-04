b475e2a9ec7def30ec11af16fc5343c3a5f8f857
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2024-01-04 15:11:15 -0300

    Commit start routes

--- aulas/02-ignite-timer/src/App.tsx
+++ aulas/02-ignite-timer/src/App.tsx
@@ -1,16 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="normal" />
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
