198f73e327ef9999729cd57e3c92087face55c59
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2024-03-20 10:16:40 -0300

    Commit pizza shop

---
+++ aulas/pizzashop-web/src/pages/app/orders/order-table-filters.tsx
@@ -0,0 +1,35 @@
+import { SelectContent, SelectItem, SelectTrigger, SelectValue, Select } from '@/components/ui/select';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search, X } from "lucide-react";
+
+export function OrderTableFilters() {
+    return (
+        <form className="flex items-center gap-2">
+            <span className="text-sm font-semibold">Filtros:</span>
+            <Input placeholder="ID do Pedido" className="h-8 w-auto" />
+            <Input placeholder="Nome do Cliente" className="h-8 w-[320px]" />
+            <Select defaultValue="all">
+                <SelectTrigger className="h-8 w-[180px]">
+                    <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="all">Todos Status</SelectItem>
+                    <SelectItem value="pending">Pendente</SelectItem>
+                    <SelectItem value="canceled">Cancelado</SelectItem>
+                    <SelectItem value="processing">Em preparo</SelectItem>
+                    <SelectItem value="delivering">Em Entrega</SelectItem>
+                    <SelectItem value="delivered">Entregue</SelectItem>
+                </SelectContent>
+            </Select>
+            <Button type="submit" variant={'secondary'} size={'xs'}>
+                <Search className="h-4 w-4 mr-2" />
+                Filtrar Resultados
+            </Button>
+            <Button type="button" variant={'outline'} size={'xs'}>
+                <X className="h-4 w-4 mr-2" />
+                Remover Filtros
+            </Button>
+        </form>
+    )
+}
--- aulas/pizzashop-web/src/pages/auth/sign-in.tsx
+++ aulas/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -6,6 +6,7 @@ import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -21,24 +22,31 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
 
-    toast.success('Enviamos um link de autenticação para seu e-mail', {
-      action: {
-        label: 'reenviar',
-        onClick: () => {
-          handleSignIn(data)
-          handleSignIn(data)
+      toast.success('Enviamos um link de autenticação para seu e-mail', {
+        action: {
+          label: 'reenviar',
+          onClick: () => {
+            handleSignIn(data)           
+          },
         },
-      },
-    })
+      })
+    } catch (error) {
+      toast.error('Erro ao fazer autenticação!')
+    }
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button asChild className="absolute right-4 top-8" variant={'ghost'}>
+          <Link to={"/sign-up"}>
+            Novo Estabelecimento
+          </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
