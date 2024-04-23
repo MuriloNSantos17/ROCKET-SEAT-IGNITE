e025fb6fbc92c6196572a4b320afa09395f398bd
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2024-04-23 12:06:30 -0300

    Commit start tests

--- aulas/pizzashop-web/vite.config.ts
+++ aulas/pizzashop-web/vite.config.ts
@@ -2,6 +2,8 @@ import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
+import type { UserConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +13,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+    environment: 'happy-dom'
+  }
 })
