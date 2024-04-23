c3fb5ed8ccae5c02e5bceeccf60c427f2e7cb376
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2024-04-23 00:35:58 -0300

    Commit adjust chart

--- aulas/pizzashop-web/src/pages/app/dashboard/components/revenue-chart.tsx
+++ aulas/pizzashop-web/src/pages/app/dashboard/components/revenue-chart.tsx
@@ -10,12 +10,35 @@ import {
 } from 'recharts'
 import { useQuery } from "@tanstack/react-query";
 import { getDailyRevenueInPeriod } from "@/api/get-daily-revenue-in-period";
+import { Label } from "@/components/ui/label";
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { useMemo, useState } from "react";
+import { DateRange } from "react-day-picker";
+import { subDays } from "date-fns";
 
 export function RevenueChart() {
+    const [dateRange, setDateRange] = useState<DateRange | undefined>(
+        {
+            from: subDays(new Date(), 7),
+            to: new Date()
+        })
+
     const { data: dailyRevenueInPeriod } = useQuery({
-        queryKey: ['metrics', 'daily-revenue-in-period'],
-        queryFn: getDailyRevenueInPeriod,
+        queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+        queryFn: () => getDailyRevenueInPeriod({
+            from: dateRange?.from,
+            to: dateRange?.to
+        }),
     })
+
+    const chartData = useMemo(() => {
+        return dailyRevenueInPeriod?.map(chartItem => {
+            return {
+                date: chartItem.date,
+                receipt: chartItem.receipt / 100
+            }
+        })
+    }, [dailyRevenueInPeriod])
 
     return (
         <Card className="col-span-6">
@@ -24,12 +47,16 @@ export function RevenueChart() {
                     <CardTitle className="text-base font-medium">Receita no Período</CardTitle>
                     <CardDescription>Receita Diária no Período</CardDescription>
                 </div>
+                <div className="flex items-center gap-3">
+                    <Label>Período</Label>
+                    <DateRangePicker date={dateRange} onDateChange={setDateRange} />
+                </div>
             </CardHeader>
             <CardContent>
                 {
-                    dailyRevenueInPeriod && (
+                    chartData && (
                         <ResponsiveContainer width={'100%'} height={240}>
-                            <LineChart data={dailyRevenueInPeriod} style={{ fontSize: 12 }}>
+                            <LineChart data={chartData} style={{ fontSize: 12 }}>
                                 <YAxis stroke="#888" axisLine={false} tickLine={false} width={80} tickFormatter={(value: number) => value.toLocaleString('pt-BR', {
                                     style: 'currency',
                                     currency: 'BRL'
