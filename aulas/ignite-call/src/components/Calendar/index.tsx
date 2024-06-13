7022375b5b2d923b2e42a5ea3b427298f56dfbfd
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2024-06-13 08:37:14 -0300

    Commit validates on dates

--- aulas/ignite-call/src/components/Calendar/index.tsx
+++ aulas/ignite-call/src/components/Calendar/index.tsx
@@ -21,6 +21,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
     blockedWeekDays: number[]
+    blockedDates: number[]
 }
 
 interface CalendarProps {
@@ -98,7 +99,9 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
                     date,
                     disabled:
                         date.endOf('day').isBefore(new Date()) ||
-                        blockedDates.blockedWeekDays.includes(date.get('day')),
+                        blockedDates.blockedWeekDays.includes(date.get('day')) ||
+                        blockedDates.blockedDates.includes(date.get('date'))
+                    ,
                 }
             }),
             ...nextMonthFillArray.map((date) => {
